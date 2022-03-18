90671ddc767577e2041ba4be1260c8fed64b0ab6
chuanyueshen <47014179+chuanyueshen@users.noreply.github.com>
2022-03-18 15:02:20 -0500

    Removed secret key

--- HitchHiking/settings.py
+++ HitchHiking/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oq3lxsn*x9!^wf*7ln44&-(2x$z@w3rlpo&-brogfs)_gg#y!p'
+SECRET_KEY = 'django-insecure-xxxxxxxxxxxxxxxx' # removed original secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
